ddc8619330198b00f119e33c43a53bfcf498a5b4
davidsaenz2506 <davidsaenz976@gmail.com>
2024-05-20 04:29:35 -0500

    Allow cors from everywhere

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: '*',
+    },
+  });
   await app.listen(5000);
 }
 
